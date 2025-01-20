7c65b0ab5ec7a7addb0ad47b34bf52711cb6206d
Michael Musílek <michaelm1@email.cz>
2025-01-20 15:40:37 +0100

    random generator

--- script.js
+++ script.js
@@ -5,14 +5,27 @@ const pocetMat = document.getElementById("pocetMat");
 
 // kód vykreslení obrázků
 const pat = "<img src='img/pat.png'>";
+const mat = "<img src='img/mat.png'>";
 
 let kodFigurek = "";
 let i=0;
-while (i<10) {
-  i++;
-  kodFigurek += "<div>" + pat + "</div>";
+let = pPat = 0
+let = pMat = 0
+let patMat = false;
+while (i<Math.random()*21) {
+    i++;
+    if (Math.random()<0.5) {
+    kodFigurek += "<div>" + pat + "</div>";
+    pPat++;
+  }
+  else {
+    kodFigurek += "<div>" + mat + "</div>";
+    pMat++;
+  }
+    patMat=!patMat;
 }
 figurky.innerHTML = kodFigurek; // vloží kód řady obrázků, kterou požadujeme vykreslit
-pocetPat.innerText = i;         // vypíše počet vykreslených figurek Pata (všech deset)
-pocetMat.innerText = 0;         // vypíše počet vykreslenách figurek Mata (žádný)
+pocetPat.innerText = pPat;         // vypíše počet vykreslených figurek Pata (všech deset)
+pocetMat.innerText = pMat;         // vypíše počet vykreslenách figurek Mata (žádný)
+
 
